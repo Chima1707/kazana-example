894881fcf01b6477cbd0bc1be69e96957c049f1b
Gregor Martynus <gregor@martynus.net>
2015-08-18 18:14:56 -0400

    style: standard

--- index.js
+++ index.js
@@ -11,23 +11,23 @@ module.exports = {
     method: 'GET',
     path: '/indices/movementsByCategory',
     handler: function (request, reply) {
-      var server = request.connection.server;
+      var server = request.connection.server
       var store = server.methods.getStore({
         name: 'integrated-data',
         auth: request
-      });
+      })
 
       store.db.query('movementsByCategory', {
         group_level: 1
       })
         .then(function (response) {
           var movementsByCategory = response.rows.reduce(function (map, row) {
-            map[row.key] = row.value.amount;
-            return map;
-          }, {});
-          reply(JSON.stringify(movementsByCategory, null, 4));
+            map[row.key] = row.value.amount
+            return map
+          }, {})
+          reply(JSON.stringify(movementsByCategory, null, 4))
         })
-        .catch(reply.pouchdbError);
+        .catch(reply.pouchdbError)
     }
   }],
 
@@ -38,13 +38,13 @@ module.exports = {
         version: '1.0.0',
         /* globals emit */
         map: function (integrated) {
-          emit(integrated.data.category, integrated.data.amount);
+          emit(integrated.data.category, integrated.data.amount)
         },
         /* globals sum */
         reduce: function (keys, values) {
           return {
             amount: sum(values)
-          };
+          }
         }
       }
     }
@@ -53,18 +53,18 @@ module.exports = {
   // transformation scripts by dataReport.type => [indicator.type, handler]
   transform: {
     'bookkeeping': ['movement', function (server, dataReport, callback) {
-      var lines = dataReport.data.split(/\n/);
-      var integrated = [];
-      var matches;
+      var lines = dataReport.data.split(/\n/)
+      var integrated = []
+      var matches
       for (var i = 0; i < lines.length; i++) {
-        matches = lines[i].match(/^([+-]?[\d\.]+)\s*(.*)$/);
-        if (!matches) return callback(new Error('Data could not be parsed'));
+        matches = lines[i].match(/^([+-]?[\d\.]+)\s*(.*)$/)
+        if (!matches) return callback(new Error('Data could not be parsed'))
         integrated.push({
           amount: parseFloat(matches[1]),
           category: matches[2] || 'not set'
-        });
+        })
       }
-      callback(null, integrated);
+      callback(null, integrated)
     }]
   }
-};
+}
